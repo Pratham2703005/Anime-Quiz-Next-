3e709e29ec4ce8b77514191c16d53601704e539d
Pratham Israni <pk2732004@gmail.com>
2025-04-03 22:25:47 +0530

    api typescript fixes

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,7 +1,14 @@
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-
+type ResponseAllUsersType = {
+  id: string;
+    username: string;
+    password: string;
+    profilePic: string;
+    coins: number;
+    ranking: number;
+}
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
@@ -10,7 +17,7 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ message: "Username and password required" }, { status: 400 });
     }
 
-    let user = await prisma.user.findUnique({ where: { username: body.username } });
+    let user:ResponseAllUsersType = await prisma.user.findUnique({ where: { username: body.username } }) as ResponseAllUsersType;
 
     if (!user) {
       user = await prisma.user.create({
@@ -18,16 +25,16 @@ export async function POST(req: NextRequest) {
           username: body.username,
           password: body.password,
           profilePic: `https://robohash.org/${body.username}.png`,
-          coins: 0
+          coins: 0,
         },
-      });
+      }) as ResponseAllUsersType;
     } else if (user.password !== body.password) {
       return NextResponse.json({ message: "Invalid credentials" }, { status: 401 });
     }
 
-    const allUsers = await prisma.user.findMany({
+    const allUsers : ResponseAllUsersType[] = await prisma.user.findMany({
       orderBy:{coins:"desc"}
-    })
+    }) as ResponseAllUsersType[]
     allUsers.map((u,index)=>{
       if(user.username === u.username){
         user.ranking = index+1;
